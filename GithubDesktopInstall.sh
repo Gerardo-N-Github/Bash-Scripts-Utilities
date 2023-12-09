a4c216250f480fa154b2301cce95ca130add807a
Gerardo-N-Github <92547540+Gerardo-N-Github@users.noreply.github.com>
2023-12-08 21:11:06 -0300

    Create GithubDesktopInstall.sh

---
+++ GithubDesktopInstall.sh
@@ -0,0 +1,5 @@
+wget https://github.com/shiftkey/desktop/releases/download/release-2.9.0-linux2/GitHubDesktop-linux-2.9.0-linux2.deb
+
+sudo apt-get install gdebi-core
+
+sudo gdebi GitHubDesktop-linux-2.9.0-linux2.deb
