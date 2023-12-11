fb6f9f2587627aac35a7967255bd2d6beb002c3e
Gerardo-N-Github <92547540+Gerardo-N-Github@users.noreply.github.com>
2023-12-11 19:45:12 -0300

    Add Debian Scripts

---
+++ Debian/lxdeInstaller.sh
@@ -0,0 +1,1 @@
+sudo apt install lxde-core
---
+++ Debian/netbeans20-01Installer.sh
@@ -0,0 +1,7 @@
+sudo apt install wget
+sudo apt-get install openjdk-17-jdk
+
+wget https://dlcdn.apache.org/netbeans/netbeans-installers/20/apache-netbeans_20-1_all.deb
+
+sudo apt install ./apache-netbeans_20-1_all.deb
+
